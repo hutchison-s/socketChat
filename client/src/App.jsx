739fca61bef2a6514989a8cb23296b6fb6d5f985
Steven Hutchison <hutchison.music@gmail.com>
2023-05-22 21:59:26 -0500

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,135 @@
+import { useState, useEffect } from 'react'
+import {socket} from './socket';
+import './App.css'
+
+function App() {
+  const [myUsernames, setMyUsernames] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(1);
+  const [allUsernames, setAllUsernames] = useState([]);
+  const [chatLog, setChatLog] = useState([]);
+  const [isNamed, setIsNamed] = useState(false)
+  const [isConnected, setIsConnected] = useState(socket.connected)
+  const [error, setError] = useState("")
+
+  
+  useEffect(()=>{
+    socket.on("connect", ()=>{
+      setIsConnected(true); 
+      console.log(socket.id)
+    })
+    socket.on("disconnect", ()=>{
+      setIsConnected(false);
+      socket.emit("erase names", [...myUsernames])
+    })
+    socket.on("update", (msgs, num)=>{
+      setChatLog(msgs);
+      setTotalUsers(num);
+      document.getElementById('chatLog').scrollTop = document.getElementById('chatLog').scrollHeight
+    })
+    socket.on("update usernames", (updatedNames) => {
+      setAllUsernames(updatedNames)
+    })
+  }, [])
+
+  function sendMsg(e) {
+    e.preventDefault();
+    let newMSG = e.target.newMSG.value
+    if (newMSG.length < 1) {
+      return;
+    }
+    socket.emit("msg", newMSG);
+    e.target.reset()
+  }
+
+  function handleName(e, callback) {
+    e.preventDefault()
+    let newName = e.target.userName.value;
+    if (allUsernames.includes(newName) && !myUsernames.includes(newName)) {
+      setError("User already exists")
+      e.target.userName.style.backgroundColor = "red";
+      setTimeout(()=>{
+        e.target.userName.style.backgroundColor = "rgba(0,0,0,0.4)";
+        setError("")
+      }, 2000)
+    } else if (newName.length < 2) {
+      setError("Username must be at least 2 characters long")
+      e.target.userName.style.backgroundColor = "red";
+      setTimeout(()=>{
+        e.target.userName.style.backgroundColor = "rgba(0,0,0,0.4)";
+        setError("")
+      }, 2000)
+    } else {
+      callback(newName)
+    }
+    e.target.reset()
+  }
+
+  function newNameFunc(newName) {
+      socket.emit("join lobby", newName)
+      setMyUsernames([newName]);
+      setIsNamed(true)
+  }
+
+  function changNameFunc(newName) {
+      socket.emit("change username", newName);
+      setMyUsernames([newName, ...myUsernames]);
+  }
+  
+  return (
+    <>
+      {!isNamed
+        ? <>
+            <div id='spinSocket'></div>
+            
+            <header className='pageHead'>
+              <div id='welcomeBox'>
+                <h1 id='welcomeTitle'>Choose a name to start Chatting!</h1>
+                <p className='errorMsg'>{error}</p>
+              </div>
+              <form id='nameHandler' onSubmit={(e) => {handleName(e, newNameFunc)}}> 
+                <input id='userName' name='userName' placeholder='Choose username...' />
+                <button type='submit'>Enter Chat</button>
+              </form>
+              
+              
+            </header>
+          </>
+        : <>
+            <header className='pageHead'>
+              <div id='welcomeBox'>
+                <h1 id='welcomeTitle'>{isConnected ? `Chatting as ${myUsernames[0]}` : "Disconnected"}</h1>
+                <small id='welcomeMsg'>Welcome to our Socket Space!</small><br></br>
+                <small id='userCount'>{totalUsers} {totalUsers > 1 ? "users" : "user"} chatting</small>
+              </div>
+              <form id='nameHandler' onSubmit={(e)=> {handleName(e, changNameFunc)}}> 
+                <input id='userName' name='userName' placeholder='Change username...' />
+                <button type='submit'>Change</button>
+              </form>
+              <p className='errorMsg'>{error}</p>
+            </header>
+            <section id='chatLog'>{
+              chatLog.map((x, idx) => (
+                  myUsernames.includes(x.user)
+                    ? <p key={idx} className="myMsg">{x.msg}</p>
+                    : x.user === ''
+                      ? <p key={idx} className='hostMsg'>{x.msg}</p>
+                      : <p key={idx}>{x.user}:&nbsp;{x.msg}</p>
+              ))}
+            </section>
+            <form id='newMsgHandler' onSubmit={sendMsg}>
+              <textarea id='newMSG' type='submit' onKeyUp={(e)=>{
+                if (e.key === 'Enter') {
+                  sendMsg({
+                    preventDefault: ()=>{null}, 
+                    target: document.forms['newMsgHandler']
+                  })
+                }
+              }}/>
+              <button type='submit'>Send</button>
+            </form>
+          </>}
+    </>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,83 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 5000;
+const http = require('http')
+const { Server } = require('socket.io')
+const cors = require('cors');
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ["GET", "POST"]
+    }
+})
+
+server.listen(PORT, ()=>{
+    console.log("listening on "+PORT)
+})
+
+const messages = [];
+let userCount = 0;
+const userNames = [];
+
+io.on("connect", (socket) => {
+    console.log("established new connection");
+    console.log(io.engine.clientsCount, "users connected.")
+    io.emit("update usernames", [...userNames])
+    
+    socket.on("join lobby", (name)=>{
+        socket.data.userName = [name];
+        messages.push({user: '', msg: `${socket.data.userName[0]} has entered the chat`});
+        io.emit("update", [...messages], io.engine.clientsCount);
+        socket.data.userName.unshift(name);
+        userNames.push(name);
+        io.emit("update usernames", [...userNames])
+    })
+    socket.on("msg", (incoming)=>{
+        messages.push({user: socket.data.userName[0], msg: incoming});
+        messages.length >= 50 ? messages.shift() : null;
+        io.emit("update", [...messages], io.engine.clientsCount);
+    })
+    socket.on("change username", (newName) => {
+        messages.push({user: '', msg: `${socket.data.userName[0]} changed name to ${newName}`});
+        io.emit("update", [...messages], io.engine.clientsCount)
+        socket.data.userName.unshift(newName);
+        userNames.push(newName);
+        io.emit("update usernames", [...userNames])
+    })
+    socket.on("erase names", (names) => {
+        for (let name of names) {
+            userNames.splice(userNames.indexOf(name), 1);
+        }
+        io.emit("update usernames", [...userNames])
+    })
+    socket.on("disconnect", ()=>{
+        console.log(io.engine.clientsCount, "users connected.")
+        if (io.engine.clientsCount === 0) {
+            for (let i=0; i<messages.length; i++) {
+                messages.pop();
+            }
+            for (let i=0; i<userNames.length; i++) {
+                userNames.pop();
+            }
+            userCount = 0;
+        } else {
+            if (socket.data.hasOwnProperty('userName')) {
+                messages.push({user: '', msg: `${socket.data.userName[0]} has left the chat`});
+                io.emit("update", [...messages], io.engine.clientsCount)
+                for (let name of socket.data.userName) {
+                    userNames.splice(userNames.indexOf(name), 1)
+                }
+                io.emit("update usernames", [...userNames])
+            }
+            
+        }
+        
+        
+    })
+        
+})
